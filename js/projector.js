99b2aecf0f1d014b9e36b06b0fd2bbded3b2ab4f
Vadim I. <vadign00@gmail.com>
2024-07-09 21:14:22 +0200

    Tabs -> spaces

--- js/projector.js
+++ js/projector.js
@@ -18,8 +18,8 @@ let openingDuration = 3000;
 let cardDrawDuration = 1000;
 let fadeDuration = 500;
 let animationOptions = {
-	fill: 'forwards',
-	easing: 'ease-in-out',
+    fill: 'forwards',
+    easing: 'ease-in-out',
 };
 
 let decor = document.getElementById('decor');
@@ -44,7 +44,7 @@ let themeReadingTitle = document.getElementById('theme-reading-title');
 let cardModal = document.getElementById('card-modal');
 let cardModalImage = document.getElementById('card-modal-image');
 let cardInfoElements = detailsMenu.querySelectorAll(
-	'#card-name, #card-alt-name, #general-reading, #theme-reading'
+    '#card-name, #card-alt-name, #general-reading, #theme-reading'
 );
 
 let cards = Array.prototype.slice.call(table.getElementsByClassName('card'));
@@ -58,264 +58,268 @@ let show = (el) => el.classList.remove('hidden');
 let toggle = (el, condition) => el.classList.toggle('hidden', !condition);
 
 function getPosition(el) {
-	let rect = el.getBoundingClientRect();
-	return { left: rect.x + 'px', top: rect.y + 'px' };
+    let rect = el.getBoundingClientRect();
+    return { left: rect.x + 'px', top: rect.y + 'px' };
 }
 
 function fadeOut(el, duration, remove) {
-	el.animate(
-		{ opacity: [getComputedStyle(el).opacity, 0] },
-		Object.assign({ duration }, animationOptions)
-	).onfinish = (event) => {
-		if (remove) {
-			el.remove();
-		} else {
-			event.target.cancel();
-			hide(el);
-		}
-	};
+    el.animate(
+        { opacity: [getComputedStyle(el).opacity, 0] },
+        Object.assign({ duration }, animationOptions)
+    ).onfinish = (event) => {
+        if (remove) {
+            el.remove();
+        } else {
+            event.target.cancel();
+            hide(el);
+        }
+    };
 }
 
 function slideUp(query, duration, callback) {
-	let els = app.querySelectorAll(query);
-	Promise.all(Array.prototype.map.call(els, (el) => new Promise((resolve) => {
-		let compStyle = getComputedStyle(el);
-		let keyframes = {
-			height: [el.offsetHeight + 'px', 0],
-			paddingTop: [compStyle.paddingTop, 0],
-			paddingBottom: [compStyle.paddingBottom, 0]
-		};
-		el.animate(
-			keyframes,
-			Object.assign({ duration }, animationOptions)
-		).onfinish = () => {
-			el.remove();
-			resolve();
-		};
-	}))).then(callback);
-	els.forEach((el) => el.style.overflow = 'hidden');
+    let els = app.querySelectorAll(query);
+    Promise.all(
+        Array.prototype.map.call(els, (el) =>
+            new Promise((resolve) => {
+                let compStyle = getComputedStyle(el);
+                let keyframes = {
+                    height: [el.offsetHeight + 'px', 0],
+                    paddingTop: [compStyle.paddingTop, 0],
+                    paddingBottom: [compStyle.paddingBottom, 0]
+                };
+                el.animate(
+                    keyframes,
+                    Object.assign({ duration }, animationOptions)
+                ).onfinish = () => {
+                    el.remove();
+                    resolve();
+                };
+            })
+        )
+    ).then(callback);
+    els.forEach((el) => el.style.overflow = 'hidden');
 }
 
 function moveTo(el, source, target, duration, callback) {
-	let animation = el.animate(
-		[getPosition(source), getPosition(target)],
-		Object.assign({ duration }, animationOptions)
-	);
+    let animation = el.animate(
+        [getPosition(source), getPosition(target)],
+        Object.assign({ duration }, animationOptions)
+    );
 
-	animation.onfinish = callback;
-	return animation;
+    animation.onfinish = callback;
+    return animation;
 }
 
 function runOnLoad(el, callback) {
-	el.complete ? callback() : el.addEventListener('load', callback, { once: true });
+    el.complete ? callback() : el.addEventListener('load', callback, { once: true });
 }
 
 function startApp(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	document.getElementById('btn-show-details').addEventListener('click', showDetails);
-	document.getElementById('btn-hide-details').addEventListener('click', hideDetails);
+    document.getElementById('btn-show-details').addEventListener('click', showDetails);
+    document.getElementById('btn-hide-details').addEventListener('click', hideDetails);
 
-	table.addEventListener('click', handleTableClick);
-	resetButton.addEventListener('click', resetTable);
-	app.addEventListener('change', resetTable);
+    table.addEventListener('click', handleTableClick);
+    resetButton.addEventListener('click', resetTable);
+    app.addEventListener('change', resetTable);
 
-	detailsImage.addEventListener('click', () => show(cardModal));
-	cardModal.addEventListener('click', () => hide(cardModal));
+    detailsImage.addEventListener('click', () => show(cardModal));
+    cardModal.addEventListener('click', () => hide(cardModal));
 
-	deck.addEventListener('load', () => {
-		show(deck);
-		hide(deckLoadingIcon);
-	});
+    deck.addEventListener('load', () => {
+        show(deck);
+        hide(deckLoadingIcon);
+    });
 
-	spreadSelect.addEventListener('change', handleSpreadChange);
-	themeSelect.addEventListener('change', handleThemeChange);
-	deckSelect.addEventListener('change', handleDeckChange);
-	handleSpreadChange();
-	handleThemeChange();
-	handleDeckChange();
+    spreadSelect.addEventListener('change', handleSpreadChange);
+    themeSelect.addEventListener('change', handleThemeChange);
+    deckSelect.addEventListener('change', handleDeckChange);
+    handleSpreadChange();
+    handleThemeChange();
+    handleDeckChange();
 
-	slideUp('#header, #intro', openingDuration, () => {
-		showDetails();
-		app.querySelectorAll('.button-bar .button').forEach(show);
-		deck.addEventListener('click', drawCard);
-	});
+    slideUp('#header, #intro', openingDuration, () => {
+        showDetails();
+        app.querySelectorAll('.button-bar .button').forEach(show);
+        deck.addEventListener('click', drawCard);
+    });
 
-	show(table);
-	hide(startButton);
+    show(table);
+    hide(startButton);
 }
 
 function handleSpreadChangeInitial() {
-	let option = spreadSelect.selectedOptions[0];
-	let theme = option.dataset.theme;
+    let option = spreadSelect.selectedOptions[0];
+    let theme = option.dataset.theme;
 
-	spreadName = option.text.split(' (')[0];
-	themeSelect.disabled = theme;
-	if (theme) themeSelect.value = theme;
+    spreadName = option.text.split(' (')[0];
+    themeSelect.disabled = theme;
+    if (theme) themeSelect.value = theme;
 }
 
 function handleSpreadChange(event) {
-	let spreadId = spreadSelect.value;
+    let spreadId = spreadSelect.value;
 
-	if (themeSelect.disabled && event) handleThemeChange();
+    if (themeSelect.disabled && event) handleThemeChange();
 
-	app.className = 'sp-' + spreadId;
-	titles = data.titles[spreadId] || fallbackTitles;
-	spreadDetails.textContent = data.details[spreadId];
-	positionList.textContent = data.positions[spreadId];
-	toggle(positionListTitle, positionList.textContent);
+    app.className = 'sp-' + spreadId;
+    titles = data.titles[spreadId] || fallbackTitles;
+    spreadDetails.textContent = data.details[spreadId];
+    positionList.textContent = data.positions[spreadId];
+    toggle(positionListTitle, positionList.textContent);
 }
 
 function handleThemeChange() {
-	readings = data.readings[themeSelect.value];
-	toggle(themeReadingTitle, readings);
+    readings = data.readings[themeSelect.value];
+    toggle(themeReadingTitle, readings);
 }
 
 function handleDeckChange() {
-	let deckId = deckSelect.value;
+    let deckId = deckSelect.value;
 
-	hide(deck);
-	show(deckLoadingIcon);
+    hide(deck);
+    show(deckLoadingIcon);
 
-	meanings = data.meanings[deckId] || data.meanings.normal;
-	altSuits = data.altSuits[deckId];
-	altRanks = data.altRanks[deckId];
-	extraMajors = data.extraMajors[deckId];
+    meanings = data.meanings[deckId] || data.meanings.normal;
+    altSuits = data.altSuits[deckId];
+    altRanks = data.altRanks[deckId];
+    extraMajors = data.extraMajors[deckId];
 
-	deckSize = 78;
-	if (extraMajors) deckSize += extraMajors.length;
+    deckSize = 78;
+    if (extraMajors) deckSize += extraMajors.length;
 
-	isClassic = deckSelect.selectedOptions[0].hasAttribute('data-classic');
-	deckPath = 'img/' + deckId + '/';
-	deck.src = animatedCardBase.src = deckPath + 'back.jpg';
+    isClassic = deckSelect.selectedOptions[0].hasAttribute('data-classic');
+    deckPath = 'img/' + deckId + '/';
+    deck.src = animatedCardBase.src = deckPath + 'back.jpg';
 }
 
 function handleTableClick(event) {
-	let slot = cards.indexOf(event.target);
+    let slot = cards.indexOf(event.target);
 
-	if (slot >= 0) {
-		showDetails(slot);
-		event.target.classList.add('card-selected');
-	}
+    if (slot >= 0) {
+        showDetails(slot);
+        event.target.classList.add('card-selected');
+    }
 }
 
 function resetTable(event) {
-	drawnCards = [];
-	descriptions = [];
+    drawnCards = [];
+    descriptions = [];
 
-	if (event.target != deckSelect) show(deck);
-	hide(resetButton);
-	showSpreadInfo();
-	deselect();
+    if (event.target != deckSelect) show(deck);
+    hide(resetButton);
+    showSpreadInfo();
+    deselect();
 
-	for (let el of animatedCards) el.dispatchEvent(new Event('table-reset'));
-	cards.forEach((el) => fadeOut(el, fadeDuration));
+    for (let el of animatedCards) el.dispatchEvent(new Event('table-reset'));
+    cards.forEach((el) => fadeOut(el, fadeDuration));
 }
 
 function createDescription(id) {
-	let adjustedId = id;
-	let name;
-	let altName;
+    let adjustedId = id;
+    let name;
+    let altName;
 
-	if (id > 21) {
-		let suit = Math.floor((id - 22) / 14);
-		let rank = id - 22 - 14 * suit;
+    if (id > 21) {
+        let suit = Math.floor((id - 22) / 14);
+        let rank = id - 22 - 14 * suit;
 
-		if (suit < 4) {
-			let rankName = data.ranks[rank];
-			let suitName = data.suits[suit];
+        if (suit < 4) {
+            let rankName = data.ranks[rank];
+            let suitName = data.suits[suit];
 
-			altName = rankName + ' ' + suitName;
+            altName = rankName + ' ' + suitName;
 
-			if (altRanks && rank > 9) rankName = altRanks[rank - 10];
-			if (altSuits) suitName = altSuits[suit];
+            if (altRanks && rank > 9) rankName = altRanks[rank - 10];
+            if (altSuits) suitName = altSuits[suit];
 
-			name = rankName + ' ' + suitName;
-			if (name == altName) altName = '';
-		} else {
-			name = extraMajors[rank];
-		}
-	} else {
-		if ((id == 8 || id == 11) && isClassic) adjustedId = 19 - id;
-		name = data.roman[adjustedId] + ' ' + data.major[id];
-	}
+            name = rankName + ' ' + suitName;
+            if (name == altName) altName = '';
+        } else {
+            name = extraMajors[rank];
+        }
+    } else {
+        if ((id == 8 || id == 11) && isClassic) adjustedId = 19 - id;
+        name = data.roman[adjustedId] + ' ' + data.major[id];
+    }
 
-	descriptions.push([name, altName, meanings[id], readings ? readings[id] : '']);
+    descriptions.push([name, altName, meanings[id], readings ? readings[id] : '']);
 
-	return adjustedId;
+    return adjustedId;
 }
 
 function drawCard() {
-	let id;
+    let id;
 
-	do id = Math.floor(Math.random() * deckSize);
-	while (drawnCards.includes(id));
+    do id = Math.floor(Math.random() * deckSize);
+    while (drawnCards.includes(id));
 
-	let slot = drawnCards.push(id) - 1;
-	let slotImg = cards[slot];
-	let animatedCardInstance = app.appendChild(animatedCardBase.cloneNode());
+    let slot = drawnCards.push(id) - 1;
+    let slotImg = cards[slot];
+    let animatedCardInstance = app.appendChild(animatedCardBase.cloneNode());
 
-	let animation = moveTo(
-		animatedCardInstance,
-		deck,
-		slotImg.parentNode,
-		cardDrawDuration,
-		() => runOnLoad(slotImg, showCard)
-	);
+    let animation = moveTo(
+        animatedCardInstance,
+        deck,
+        slotImg.parentNode,
+        cardDrawDuration,
+        () => runOnLoad(slotImg, showCard)
+    );
 
-	let showCard = () => {
-		slotImg.removeEventListener('load', showCard);
-		show(slotImg);
-		animation.pause();
-		fadeOut(animatedCardInstance, fadeDuration, true);
-	};
+    let showCard = () => {
+        slotImg.removeEventListener('load', showCard);
+        show(slotImg);
+        animation.pause();
+        fadeOut(animatedCardInstance, fadeDuration, true);
+    };
 
-	id = createDescription(id);
-	slotImg.src = deckPath + id + '.jpg';
-	animatedCardInstance.addEventListener('table-reset', showCard);
+    id = createDescription(id);
+    slotImg.src = deckPath + id + '.jpg';
+    animatedCardInstance.addEventListener('table-reset', showCard);
 
-	if (slot == titles.length - 1) {
-		show(resetButton);
-		hide(deck);
-	}
+    if (slot == titles.length - 1) {
+        show(resetButton);
+        hide(deck);
+    }
 }
 
 function showCardInfo(slot) {
-	detailsTitle.textContent = titles[slot];
-	descriptions[slot].forEach((text, i) => cardInfoElements[i].textContent = text);
-	detailsImage.src = cardModalImage.src = cards[slot].src;
-	hide(spreadInfo);
-	show(cardInfo);
+    detailsTitle.textContent = titles[slot];
+    descriptions[slot].forEach((text, i) => cardInfoElements[i].textContent = text);
+    detailsImage.src = cardModalImage.src = cards[slot].src;
+    hide(spreadInfo);
+    show(cardInfo);
 }
 
 function showSpreadInfo() {
-	detailsTitle.textContent = spreadName;
-	show(spreadInfo);
-	hide(cardInfo);
+    detailsTitle.textContent = spreadName;
+    show(spreadInfo);
+    hide(cardInfo);
 }
 
 function showDetails(slot) {
-	show(detailsMenu);
-	deselect();
-	slot >= 0 ? showCardInfo(slot) : showSpreadInfo();
+    show(detailsMenu);
+    deselect();
+    slot >= 0 ? showCardInfo(slot) : showSpreadInfo();
 }
 
 function hideDetails() {
-	hide(detailsMenu);
-	deselect();
+    hide(detailsMenu);
+    deselect();
 }
 
 function deselect() {
-	for (let el of selectedCards) el.classList.remove('card-selected');
-	detailsContent.scrollTop = 0;
+    for (let el of selectedCards) el.classList.remove('card-selected');
+    detailsContent.scrollTop = 0;
 }
 
 fetch('res/data.json?3')
 .then((response) => response.json())
 .then((json) => {
-	data = json;
-	startButton.disabled = false;
-	startButton.textContent = 'НАЧАТЬ';
+    data = json;
+    startButton.disabled = false;
+    startButton.textContent = 'НАЧАТЬ';
 });
 
 runOnLoad(decor, () => show(decor));
@@ -324,9 +328,9 @@ spreadSelect.addEventListener('change', handleSpreadChangeInitial);
 
 // ios fixes
 if ('standalone' in navigator) {
-	// prevent double-tap to zoom
-	app.addEventListener('click', () => {});
-	// fix scrolling bug
-	app.style.overflow = 'auto';
+    // prevent double-tap to zoom
+    app.addEventListener('click', () => {});
+    // fix scrolling bug
+    app.style.overflow = 'auto';
 }
 })(document.body);
