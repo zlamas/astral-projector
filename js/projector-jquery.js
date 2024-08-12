359ef2be8829fffba18389031124e6d936038afb
Vadim I. <vadign00@gmail.com>
2024-08-12 20:28:09 +0200

    projector-jquery.js: Tabs -> spaces

--- js/projector-jquery.js
+++ js/projector-jquery.js
@@ -50,210 +50,210 @@ var $deckImageElements = $deck.add($animatedCardBase);
 var $cardImageElements = $detailsImage.add($cardModalImage);
 
 function runOnLoad(el, callback) {
-	el.prop('complete') ? callback() : el.one('load', callback);
+    el.prop('complete') ? callback() : el.one('load', callback);
 }
 
 function startApp(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	$('#btn-show-details').click(showDetails);
-	$('#btn-hide-details').click(hideDetails);
+    $('#btn-show-details').click(showDetails);
+    $('#btn-hide-details').click(hideDetails);
 
-	$table.click(handleTableClick);
-	$resetButton.click(resetTable);
-	$app.change(resetTable);
+    $table.click(handleTableClick);
+    $resetButton.click(resetTable);
+    $app.change(resetTable);
 
-	$detailsImage.click(function () { $cardModal.show(); });
-	$cardModal.click(function () { $cardModal.hide(); });
+    $detailsImage.click(function () { $cardModal.show(); });
+    $cardModal.click(function () { $cardModal.hide(); });
 
-	$deck.on('load', function () {
-		$deck.show();
-		$deckLoadingIcon.hide();
-	})
+    $deck.on('load', function () {
+        $deck.show();
+        $deckLoadingIcon.hide();
+    })
 
-	$spreadSelect.on('change.data', handleSpreadChange);
-	$themeSelect.on('change.data', handleThemeChange);
-	$deckSelect.on('change.data', handleDeckChange);
-	$('select').trigger('change.data');
+    $spreadSelect.on('change.data', handleSpreadChange);
+    $themeSelect.on('change.data', handleThemeChange);
+    $deckSelect.on('change.data', handleDeckChange);
+    $('select').trigger('change.data');
 
-	$('#header, #intro').slideUp(openingDuration).promise().done(function () {
-		showDetails();
-		$('.button-bar .button').show();
-		$deck.click(drawCard);
-	});
+    $('#header, #intro').slideUp(openingDuration).promise().done(function () {
+        showDetails();
+        $('.button-bar .button').show();
+        $deck.click(drawCard);
+    });
 
-	$table.show();
-	$startButton.hide();
+    $table.show();
+    $startButton.hide();
 }
 
 function handleSpreadChangeInitial() {
-	var $option = $(this).find(':selected');
-	var theme = $option.data('theme');
+    var $option = $(this).find(':selected');
+    var theme = $option.data('theme');
 
-	spreadName = $option.text().split(' (')[0];
-	$themeSelect.prop('disabled', theme);
-	if (theme) $themeSelect.val(theme).change();
+    spreadName = $option.text().split(' (')[0];
+    $themeSelect.prop('disabled', theme);
+    if (theme) $themeSelect.val(theme).change();
 }
 
 function handleSpreadChange() {
-	var spreadId = this.value;
-	$app.prop('class', 'sp-' + spreadId);
-	titles = data.titles[spreadId] || fallbackTitles;
-	$spreadDetails.text(data.details[spreadId]);
-	$positionList.text(data.positions[spreadId] || '');
-	$positionListTitle.toggle(!!$positionList.text());
+    var spreadId = this.value;
+    $app.prop('class', 'sp-' + spreadId);
+    titles = data.titles[spreadId] || fallbackTitles;
+    $spreadDetails.text(data.details[spreadId]);
+    $positionList.text(data.positions[spreadId] || '');
+    $positionListTitle.toggle(!!$positionList.text());
 }
 
 function handleThemeChange() {
-	readings = data.readings[this.value];
-	$themeReadingTitle.toggle(!!readings);
+    readings = data.readings[this.value];
+    $themeReadingTitle.toggle(!!readings);
 }
 
 function handleDeckChange() {
-	var deckId = this.value;
+    var deckId = this.value;
 
-	$deck.hide();
-	$deckLoadingIcon.show();
+    $deck.hide();
+    $deckLoadingIcon.show();
 
-	meanings = data.meanings[deckId] || data.meanings.normal;
-	altRanks = data.altRanks[deckId];
-	altSuits = data.altSuits[deckId];
-	extraMajors = data.extraMajors[deckId];
+    meanings = data.meanings[deckId] || data.meanings.normal;
+    altRanks = data.altRanks[deckId];
+    altSuits = data.altSuits[deckId];
+    extraMajors = data.extraMajors[deckId];
 
-	deckSize = 78;
-	if (extraMajors) deckSize += extraMajors.length;
+    deckSize = 78;
+    if (extraMajors) deckSize += extraMajors.length;
 
-	isClassic = $(this).find(':selected[data-classic]').length;
-	deckPath = 'img/' + deckId + '/';
-	$deckImageElements.prop('src', deckPath + 'back.jpg');
+    isClassic = $(this).find(':selected[data-classic]').length;
+    deckPath = 'img/' + deckId + '/';
+    $deckImageElements.prop('src', deckPath + 'back.jpg');
 }
 
 function handleTableClick(event) {
-	var slot = $cards.index(event.target);
+    var slot = $cards.index(event.target);
 
-	if (slot >= 0) {
-		showDetails(slot);
-		$(event.target).addClass('card-selected');
-	}
+    if (slot >= 0) {
+        showDetails(slot);
+        $(event.target).addClass('card-selected');
+    }
 }
 
 function resetTable(event) {
-	drawnCards = [];
-	descriptions = [];
+    drawnCards = [];
+    descriptions = [];
 
-	if (!$(event.target).is($deckSelect)) $deck.show();
-	$resetButton.hide();
-	showSpreadInfo();
-	deselect();
+    if (!$(event.target).is($deckSelect)) $deck.show();
+    $resetButton.hide();
+    showSpreadInfo();
+    deselect();
 
-	$('.card-animated')
-		.stop(true)
-		.fadeOut(fadeDuration, function () { $(this).remove(); });
+    $('.card-animated')
+        .stop(true)
+        .fadeOut(fadeDuration, function () { $(this).remove(); });
 
-	$cards.finish().fadeOut(fadeDuration);
+    $cards.finish().fadeOut(fadeDuration);
 }
 
 function createDescription(id) {
-	var adjustedId = id;
-	var name;
-	var altName;
+    var adjustedId = id;
+    var name;
+    var altName;
 
-	if (id > 21) {
-		var suit = Math.floor((id - 22) / 14);
-		var rank = id - 22 - 14 * suit;
+    if (id > 21) {
+        var suit = Math.floor((id - 22) / 14);
+        var rank = id - 22 - 14 * suit;
 
-		if (suit < 4) {
-			var rankName = data.ranks[rank];
-			var suitName = data.suits[suit];
+        if (suit < 4) {
+            var rankName = data.ranks[rank];
+            var suitName = data.suits[suit];
 
-			altName = rankName + ' ' + suitName;
+            altName = rankName + ' ' + suitName;
 
-			if (altRanks && rank > 9) rankName = altRanks[rank - 10];
-			if (altSuits) suitName = altSuits[suit];
+            if (altRanks && rank > 9) rankName = altRanks[rank - 10];
+            if (altSuits) suitName = altSuits[suit];
 
-			name = rankName + ' ' + suitName;
-			if (name == altName) altName = '';
-		} else {
-			name = extraMajors[rank];
-		}
-	} else {
-		if ((id == 8 || id == 11) && isClassic) adjustedId = 19 - id;
-		name = data.roman[adjustedId] + ' ' + data.major[id];
-	}
+            name = rankName + ' ' + suitName;
+            if (name == altName) altName = '';
+        } else {
+            name = extraMajors[rank];
+        }
+    } else {
+        if ((id == 8 || id == 11) && isClassic) adjustedId = 19 - id;
+        name = data.roman[adjustedId] + ' ' + data.major[id];
+    }
 
-	descriptions.push([name, altName, meanings[id], readings ? readings[id] : '']);
+    descriptions.push([name, altName, meanings[id], readings ? readings[id] : '']);
 
-	return adjustedId;
+    return adjustedId;
 }
 
 function drawCard() {
-	var id;
+    var id;
 
-	do id = Math.floor(Math.random() * deckSize);
-	while (drawnCards.indexOf(id) >= 0);
+    do id = Math.floor(Math.random() * deckSize);
+    while (drawnCards.indexOf(id) >= 0);
 
-	var slot = drawnCards.push(id) - 1;
-	var $slotImg = $cards.eq(slot);
-	var $animatedCardInstance = $animatedCardBase.clone().appendTo($table);
+    var slot = drawnCards.push(id) - 1;
+    var $slotImg = $cards.eq(slot);
+    var $animatedCardInstance = $animatedCardBase.clone().appendTo($table);
 
-	var showCard = function () {
-		$slotImg.show();
-		$animatedCardInstance.fadeOut(fadeDuration, function () {
-			$(this).remove();
-		});
-	};
+    var showCard = function () {
+        $slotImg.show();
+        $animatedCardInstance.fadeOut(fadeDuration, function () {
+            $(this).remove();
+        });
+    };
 
-	id = createDescription(id);
-	$slotImg.prop('src', deckPath + id + '.jpg');
+    id = createDescription(id);
+    $slotImg.prop('src', deckPath + id + '.jpg');
 
-	$animatedCardInstance.css($deck.position()).animate(
-		$slotImg.parent().position(),
-		cardDrawDuration,
-		function () { runOnLoad($slotImg, showCard); }
-	);
+    $animatedCardInstance.css($deck.position()).animate(
+        $slotImg.parent().position(),
+        cardDrawDuration,
+        function () { runOnLoad($slotImg, showCard); }
+    );
 
-	if (slot == titles.length - 1) {
-		$resetButton.show();
-		$deck.hide();
-	}
+    if (slot == titles.length - 1) {
+        $resetButton.show();
+        $deck.hide();
+    }
 }
 
 function showCardInfo(slot) {
-	descriptions[slot].forEach(function (text, i) {
-		$cardInfoElements.eq(i).text(text || '');
-	});
+    descriptions[slot].forEach(function (text, i) {
+        $cardInfoElements.eq(i).text(text || '');
+    });
 
-	$detailsTitle.text(titles[slot]);
-	$cardImageElements.prop('src', $cards.eq(slot).prop('src'));
-	$cardInfo.show();
-	$spreadInfo.hide();
+    $detailsTitle.text(titles[slot]);
+    $cardImageElements.prop('src', $cards.eq(slot).prop('src'));
+    $cardInfo.show();
+    $spreadInfo.hide();
 }
 
 function showSpreadInfo() {
-	$detailsTitle.text(spreadName);
-	$spreadInfo.show();
-	$cardInfo.hide();
+    $detailsTitle.text(spreadName);
+    $spreadInfo.show();
+    $cardInfo.hide();
 }
 
 function showDetails(slot) {
-	$detailsMenu.show();
-	deselect();
-	slot >= 0 ? showCardInfo(slot) : showSpreadInfo();
+    $detailsMenu.show();
+    deselect();
+    slot >= 0 ? showCardInfo(slot) : showSpreadInfo();
 }
 
 function hideDetails() {
-	$detailsMenu.hide();
-	deselect();
+    $detailsMenu.hide();
+    deselect();
 }
 
 function deselect() {
-	$('.card-selected').removeClass('card-selected');
-	$detailsContent.scrollTop(0);
+    $('.card-selected').removeClass('card-selected');
+    $detailsContent.scrollTop(0);
 }
 
 $.getJSON('res/data.json?2', function (json) {
-	data = json;
-	$startButton.prop('disabled', false).text('НАЧАТЬ');
+    data = json;
+    $startButton.prop('disabled', false).text('НАЧАТЬ');
 });
 
 $('.hidden').hide().removeClass('hidden');
@@ -263,9 +263,9 @@ $spreadSelect.change(handleSpreadChangeInitial);
 
 // ios fixes
 if ('standalone' in navigator) {
-	// prevent double-tap to zoom
-	$app.click(function () {});
-	// fix scrolling bug
-	$app.css('overflow', 'auto');
+    // prevent double-tap to zoom
+    $app.click(function () {});
+    // fix scrolling bug
+    $app.css('overflow', 'auto');
 }
 })();
